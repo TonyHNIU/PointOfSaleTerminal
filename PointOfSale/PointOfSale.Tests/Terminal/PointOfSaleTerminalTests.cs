578cd936a81a83f9c35f4eb28309f5b78d8a6ba5
Anton Puhach <PugachAG@gmail.com>
2015-11-24 13:10:32 +0200

    Add tests

--- PointOfSale/PointOfSale.Tests/Terminal/PointOfSaleTerminalTests.cs
+++ PointOfSale/PointOfSale.Tests/Terminal/PointOfSaleTerminalTests.cs
@@ -1,23 +1,44 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PointOfSale.Terminal;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace PointOfSale.Tests.Terminal
 {
-    [TestClass]
+    [TestFixture]
     public class PointOfSaleTerminalTests
     {
-        const double Delta = 1e-5;
+        private const double Delta = 1e-5;
 
-        [TestMethod]
-        public void TestMethod1()
+        [TestCase("", 0.0)]
+        [TestCase("A", 1.25)]
+        [TestCase("AA", 2.5)]
+        [TestCase("AAA", 3.0)]
+        // Test cases from test statement
+        [TestCase("ABCDABA", 13.25)]
+        [TestCase("CCCCCCC", 6.0)]
+        [TestCase("ABCD", 7.25)]
+        public void ScanProductsSequence_WithDefaultPricesSet_CorrectTotalValue(string productCodes, double expectedResult)
         {
-            var terminal = new PointOfSaleTerminal(CreateDefaultProducts());
+            var terminal = new PointOfSaleTerminal(
+                new ProductsPriceSetBuilder()
+                    .AddProduct("A", 1.25, 3, 3.0)
+                    .AddProduct("B", 4.25)
+                    .AddProduct("C", 1.0, 6, 5.0)
+                    .AddProduct("D", 0.75)
+                    .GetAllProducts());
 
-            ScanStringAsChars(terminal, "ABCDABA");
+            ScanStringAsChars(terminal, productCodes);
 
-            Assert.AreEqual(terminal.CalculateTotal(), 13.25, Delta);
+            Assert.AreEqual(expectedResult, terminal.CalculateTotal(), Delta);
+        }
+
+        [Test]
+        public void ScanNonExistingProduct_WithEmptyPricesSet_ExceptionThrown()
+        {
+            var terminal = new PointOfSaleTerminal(new PointOfSaleProduct[0]);
+
+            Assert.Throws(typeof(ArgumentException), () => terminal.Scan("A"));
         }
 
         private void ScanStringAsChars(PointOfSaleTerminal terminal, string productCodes)
@@ -26,16 +47,6 @@ namespace PointOfSale.Tests.Terminal
             {
                 terminal.Scan(ch.ToString());
             }
-        }
-
-        private PointOfSaleProduct[] CreateDefaultProducts()
-        {
-            return new ProductsPriceSetBuilder()
-                    .AddProduct("A", 1.25, 3, 3.0)
-                    .AddProduct("B", 4.25)
-                    .AddProduct("C", 1.0, 6, 5.0)
-                    .AddProduct("D", 0.75)
-                    .GetAllProducts();
         }
     }
 }
